c7535acd24e8c9ce92b64fba342bd65584851214
brahamrdz <100656093+brahamrdz@users.noreply.github.com>
2025-11-14 12:01:43 -0600

    Add date utility functions for manipulation and formatting

--- dateFunctions.js
+++ dateFunctions.js
@@ -3,32 +3,47 @@ function getCurrentDate() {
 }
 
 function getYear(date) {
+    return date.getFullYear();
 }
 
 function getMonth(date) {
+    return date.getMonth() + 1; // +1 porque getMonth() devuelve 0-11
 }
 
 function getDayOfMonth(date) {
+    return date.getDate();
 }
 
 function changeYear(date, year) {
-    
+    const newDate = new Date(date);
+    newDate.setFullYear(year);
+    return newDate;
 }
 
 function addDays(date, days) {
-   
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + days);
+    return newDate;
 }
 
 function getDayOfWeek(date) {
+    const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    return days[date.getDay()];
 }
 
 function formatDate(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
 }
 
 function isBefore(date1, date2) {
+    return date1.getTime() < date2.getTime();
 }
 
 function isAfter(date1, date2) {
+    return date1.getTime() > date2.getTime();
 }
 
 module.exports = {

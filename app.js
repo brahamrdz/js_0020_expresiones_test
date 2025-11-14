6c41311521ab4164e46aa134604c0f83ed08f206
brahamrdz <100656093+brahamrdz@users.noreply.github.com>
2025-11-14 12:12:18 -0600

    add operations to functions in app.js

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
 // mathExpressions.js
 
 function add(a, b) {
-  
+    return a + b;
 }
 
 function subtract(a, b) {
+    return a - b;
 }
 
 function multiply(a, b) {
+    return a * b;
 }
 
 function divide(a, b) {
- 
+    if (b === 0) throw new Error("Division by zero is not allowed");
+    return a / b;
 }
 
 function modulus(a, b) {
-  
+    if (b === 0) throw new Error("Modulus by zero is not allowed");
+    return a % b;
 }
 
 function exponent(a, b) {
-  
+    return Math.pow(a, b);
 }
 
 function squareRoot(a) {
- 
+    if (a < 0) throw new Error("Square root of negative number is not allowed");
+    return Math.sqrt(a);
 }
 
 function cube(a) {
-  
+    return a * a * a;
 }
 
 function absolute(a) {
-  
+    return Math.abs(a);
 }
 
 function sine(degrees) {
-  
+    const radians = degrees * (Math.PI / 180);
+    return Math.sin(radians);
 }
 
 module.exports = {

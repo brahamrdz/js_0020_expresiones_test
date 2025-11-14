080a9354aa6817d9f43132b15222f7b25cc9d2c1
brahamrdz <100656093+brahamrdz@users.noreply.github.com>
2025-11-14 12:07:40 -0600

    Remove unnecessary whitespace in logic functions

--- logicFunctions.js
+++ logicFunctions.js
@@ -1,43 +1,41 @@
 function isEven(num) {
-    
+    return num % 2 === 0;
 }
 
 function isGreater(a, b) {
-    
+    return a > b;
 }
 
 function isLessOrEqual(a, b) {
-    
+    return a <= b;
 }
 
 function isInRange(value, min, max) {
-    
+    return value >= min && value <= max;
 }
 
 function areEqual(a, b) {
-    
+    return a === b;
 }
 
 function areNotEqual(a, b) {
-    
+    return a !== b;
 }
 
 function eitherOr(a, b) {
-   
-        
+    return a || b;
 }
 
 function neitherNor(a, b) {
-    
+    return !a && !b;
 }
 
 function bothAnd(a, b) {
-    
+    return a && b;
 }
 
 function exclusiveOr(a, b) {
-   
-        
+    return (a || b) && !(a && b);
 }
 
 module.exports = {
